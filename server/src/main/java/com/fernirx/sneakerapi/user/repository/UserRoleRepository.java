315fa2aa01684ecc82c68daf44b500028916bd7d
Fernirx <anhtuhungdeveloper@gmail.com>
2026-06-11 15:03:33 +0700

    feat(user): thêm deleteAllByUser vào UserRoleRepository
    Cần thiết để xóa toàn bộ role của user trước khi gán lại
    khi admin thực hiện cập nhật roles qua internal API.

--- server/src/main/java/com/fernirx/sneakerapi/user/repository/UserRoleRepository.java
+++ server/src/main/java/com/fernirx/sneakerapi/user/repository/UserRoleRepository.java
@@ -1,6 +1,9 @@
 package com.fernirx.sneakerapi.user.repository;
 
+import com.fernirx.sneakerapi.user.entity.User;
 import com.fernirx.sneakerapi.user.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {}
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+    void deleteAllByUser(User user);
+}
